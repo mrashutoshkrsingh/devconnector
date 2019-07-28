0e785c0d874c0a7b7768fd22fb02e24cbf9a2915
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2019-07-28 07:50:42 +0530

    production issue resolved

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const Posts = require("./routes/api/posts");
 const Profile = require("./routes/api/profile");
 const User = require("./routes/api/user");
 const passport = require("passport");
+const path = require("path");
 mongoose
-	.connect(db, { useNewUrlParser: true })
-	.then(() => {
-		console.log("Mongoose connected");
-	})
-	.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Mongoose connected");
+  })
+  .catch(err => console.log(err));
 
 //body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,16 +26,22 @@ app.use(passport.initialize());
 //passport config
 require("./config/passport")(passport);
 
-app.get("/", (req, res) => {
-	res.send("Hello");
-});
-
 app.use("/api/users", User);
 app.use("/api/posts", Posts);
 app.use("/api/profile", Profile);
 
+//Server static assets if production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server started at port ${port}`);
+  console.log(`Server started at port ${port}`);
 });
