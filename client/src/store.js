1b9a8a59408e8b249bed25d2d856152aeeabbbd7
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2019-07-28 08:09:27 +0530

    redux extension issues resolved

--- client/src/store.js
+++ client/src/store.js
@@ -3,13 +3,20 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 const middleware = [thunk];
 const initialState = {};
+const devTools =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
