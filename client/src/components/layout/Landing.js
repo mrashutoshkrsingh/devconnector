3a20854e667be2367ca187006045b263029aa589
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2019-07-30 10:21:48 +0530

    landing bug removed

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,36 +1,43 @@
-import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-const Landing = ({ isAuthenticated }) => {
-  if (isAuthenticated) {
-    return <Redirect to="/dashboard" />;
+class Landing extends Component {
+  componentDidMount() {
+    if (this.props.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
   }
 
-  return (
-    <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
-          <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers....
-          </p>
-          <div className="buttons">
-            <Link to="/register" className="btn btn-primary">
-              Sign Up
-            </Link>
-            <Link to="/login" className="btn btn-light">
-              Login
-            </Link>
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <p className="lead">
+                  {" "}
+                  Create a developer profile/portfolio, share posts and get help
+                  from other developers
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-  );
-};
-
+    );
+  }
+}
 Landing.propTypes = {
   isAuthenticated: PropTypes.bool
 };
